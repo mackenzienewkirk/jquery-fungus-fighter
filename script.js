b09c4783f143ef71b0e8950c2e76c6bf1eedf3f7
Mackenzie Newkirk <mackenzie.joy.newkirk@gmail.com>
2022-12-01 14:41:53 -0600

    all buttons created and working

--- script.js
+++ script.js
@@ -14,6 +14,9 @@ function onReady() {
     //testing the onReady
     console.log('All ready in the console')
     $('.attack-btn.arcane-sceptre').on('click', arcaneButton)
+    $('.attack-btn.entangle').on('click', entangleButton)
+    $('.attack-btn.dragon-blade').on('click', dragonBladeButton)
+    $('.attack-btn.star-fire').on('click', starFireButton)
     // Make sure you check the index.html file! 
     // There are lots of buttons and things ready for you to hook into here!
     
@@ -28,11 +31,39 @@ function onReady() {
 //Button for arcane-sceptre button is clicked
 //apPoints decrease, hpPoints increase
 
+//Creating the arcaneButton to decrease points
 function arcaneButton() {
     console.log('The arcaneButton is created');
     $('#ap-value').text(apPoints);
     $('#hp-value').text(hpPoints);
     apPoints -= 12;
     hpPoints -= 14; 
+}
+
+//Creating entangleButton to decrease points
+function entangleButton() {
+    console.log('The entangleButton is created');
+    $('#ap-value').text(apPoints);
+    $('#hp-value').text(hpPoints);
+    apPoints -= 23;
+    hpPoints -= 9; 
+}
+
+//Creating dragonBladeButton to decrease points
+function dragonBladeButton() {
+    console.log('The dragonBladeButton is created');
+    $('#ap-value').text(apPoints);
+    $('#hp-value').text(hpPoints);
+    apPoints -= 38;
+    hpPoints -= 47; 
+}
+
+//Creating the starFireButton to decrease points
+function starFireButton() {
+    console.log('The starFireButton is created');
+    $('#ap-value').text(apPoints);
+    $('#hp-value').text(hpPoints);
+    apPoints -= 33;
+    hpPoints -= 25; 
 }
 
